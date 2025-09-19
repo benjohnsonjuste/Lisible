fb0840d1353b1630493bed06c6e066fd04f4fa41
benjohnsonjuste <jb7management@gmail.com>
2025-09-19 07:50:47 -0400

    index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Bienvenue sur Lisible 📚</h1>
+      <p>La plateforme de lecture en streaming.</p>
+    </div>
+  );
+}
