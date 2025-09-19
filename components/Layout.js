f034d734412372c44bc5b40e473dd989c4fcc665
benjohnsonjuste <jb7management@gmail.com>
2025-09-19 15:15:55 -0400

    Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,9 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", width: "100vw", margin: 0, padding: 0 }}>
       <Navbar />
-      <main style={{ flex: 1 }}>{children}</main>
+      <main style={{ flex: 1, width: "100%" }}>{children}</main>
       <Footer />
     </div>
   );
