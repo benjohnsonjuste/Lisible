832728aa306892674f6d4324a982e7961cbd804e
benjohnsonjuste <jb7management@gmail.com>
2025-09-21 02:04:01 -0400

    Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,11 @@
-// components/Layout.js
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* ✅ Navbar en haut */}
       <Navbar />
-
-      {/* ✅ Contenu de la page */}
-      <main className="flex-1">{children}</main>
-
-      {/* ✅ Footer en bas */}
+      <main className="flex-1 mt-16">{children}</main>
       <Footer />
     </div>
   );
