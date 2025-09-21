ffdba88d82cb3c4bcd7e104d3c5e4e07e5dfc26b
benjohnsonjuste <jb7management@gmail.com>
2025-09-20 20:08:20 -0400

    Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 export default function Banner({ title, subtitle, imageUrl }) {
   return (
-    <div className="relative w-full h-[70vh]">
-      <img
+    <div className="relative w-full min-h-[70vh]">
+      <Image
         src={imageUrl}
         alt={title}
-        className="object-cover w-full h-full"
+        fill
+        priority
+        className="object-cover"
       />
       <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/80 flex flex-col items-center justify-center">
         <h1 className="text-4xl md:text-6xl text-white font-bold text-center px-4">
