1e585bc883cc8f04eb213162e24803399ddc7a24
benjohnsonjuste <jb7management@gmail.com>
2025-09-21 01:40:52 -0400

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB", // bleu standard
+        secondary: "#10B981", // vert pour accents
+        accent: "#FBBF24", // jaune accent
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        serif: ["Merriweather", "ui-serif", "Georgia"],
+      },
+    },
+  },
+  plugins: [],
+};
