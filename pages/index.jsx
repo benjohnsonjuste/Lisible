6106e69a234fc94862a586d4cbe181536a9c1e5a
benjohnsonjuste <jb7management@gmail.com>
2025-09-22 03:06:48 -0400

    index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import BrandedBackground from '../components/BrandedBackground';
+import RegistrationDialogue from '../components/RegistrationDialogue';
+
+export default function Home() {
+  return (
+    <BrandedBackground>
+      <RegistrationDialogue />
+    </BrandedBackground>
+  );
+}
