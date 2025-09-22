c73801abc1f5e69c9a698d73cecba0a34b9c8f1a
benjohnsonjuste <jb7management@gmail.com>
2025-09-22 02:48:43 -0400

    postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    'tailwindcss/nesting': {},  // Support pour l'imbrication des styles
+    tailwindcss: {},            // Tailwind CSS
+    autoprefixer: {},           // Ajoute automatiquement les préfixes CSS
+  },
+};
