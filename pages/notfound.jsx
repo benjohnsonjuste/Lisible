c495d6094380a4ce034988bc75fb198263bfc441
benjohnsonjuste <jb7management@gmail.com>
2025-09-22 03:07:25 -0400

    notfound.jsx

---
+++ pages/notfound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-4xl font-bold">404 - Page non trouvée</h1>
+    </div>
+  );
+}
