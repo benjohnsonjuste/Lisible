a938de596874ac96016e35cb5de1324093375152
benjohnsonjuste <jb7management@gmail.com>
2025-09-22 00:40:32 -0400

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,6 @@
-import "../styles/globals.css"; // Import des styles globaux
-import { useEffect } from "react";
-import { auth } from "../firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Exemple : observer l'état de l'utilisateur (facultatif)
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("Utilisateur connecté :", user.email);
-      } else {
-        console.log("Utilisateur non connecté");
-      }
-    });
-    return () => unsubscribe();
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
