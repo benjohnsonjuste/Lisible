5ecaf8ba22fb9e82c435bd220806750ad4f67586
benjohnsonjuste <jb7management@gmail.com>
2025-09-23 17:43:43 -0400

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    'tailwindcss/nesting': {},  // Support pour l'imbrication des styles
-    tailwindcss: {},            // Tailwind CSS
-    autoprefixer: {},           // Ajoute automatiquement les préfixes CSS
-  },
-};
