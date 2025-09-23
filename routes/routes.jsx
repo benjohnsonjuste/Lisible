5f764c59c621ce2ff7d71ce91762ea7835f0ee32
benjohnsonjuste <jb7management@gmail.com>
2025-09-23 17:42:00 -0400

    Delete routes directory

--- routes/routes.jsx
+++
@@ -1,9 +0,0 @@
-import Home from '../pages/index';
-import Application from '../pages/application';
-import NotFound from '../pages/notfound';
-
-export const routes = [
-  { path: '/', component: Home },
-  { path: '/application', component: Application },
-  { path: '*', component: NotFound }
-];
