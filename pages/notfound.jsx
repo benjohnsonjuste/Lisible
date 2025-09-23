2a003458c53b29e896e44f3401f5200609269d9d
benjohnsonjuste <jb7management@gmail.com>
2025-09-23 17:39:45 -0400

    Delete pages directory

--- pages/notfound.jsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-4xl font-bold">404 - Page non trouvée</h1>
-    </div>
-  );
-}
