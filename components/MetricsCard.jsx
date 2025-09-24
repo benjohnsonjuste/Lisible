b2c459c21276e6e59e2cd60d71d088bb1e3a87b7
benjohnsonjuste <jb7management@gmail.com>
2025-09-24 11:43:45 -0400

    Delete components directory

--- components/MetricsCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function MetricsCard({ title, value }) {
-  return (
-    <div className="bg-white p-4 rounded shadow">
-      <h4 className="text-sm text-gray-500">{title}</h4>
-      <div className="text-2xl font-bold">{value}</div>
-    </div>
-  );
-}
