3c94a7968c67db5b2242b1b5ef0731e90e6db17c
benjohnsonjuste <jb7management@gmail.com>
2025-09-24 02:24:24 -0400

    page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,11 @@
-import AuthDialog from "@/components/AuthDialog";
+import React from "react";
+import AuthDialog from "../../components/AuthDialog";
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <AuthDialog type="login" />
+    <div className="login-page">
+      <h1>Connexion</h1>
+      <AuthDialog />
     </div>
   );
 }
