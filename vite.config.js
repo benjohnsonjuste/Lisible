860aa9af1ea6e9ba1e78f4f28fb7e082b7989b4f
benjohnsonjuste <jb7management@gmail.com>
2025-09-24 15:38:52 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Changer le dossier de sortie de dist à build
   build: {
     outDir: "build",
-    chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 2000
   },
-  plugins: [tsconfigPaths(), react(), tagger()],
+  plugins: [tsconfigPaths(), react()],
   server: {
     port: 4028,
     host: "0.0.0.0",
-    strictPort: true,
-  },
+    strictPort: true
+  }
 });
