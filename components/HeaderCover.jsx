7b7e06bb6e5228dbae8198d6d4511487376deb84
benjohnsonjuste <jb7management@gmail.com>
2025-09-24 00:55:40 -0400

    HeaderCover.jsx

--- components/HeaderCover.jsx
+++ components/HeaderCover.jsx
@@ -1,6 +1,6 @@
 export default function HeaderCover() {
   return (
-    <header className="couverture.jpg">
+    <header className="couverture">
       <div className="container h-full flex flex-col justify-center items-center text-white">
         <h1 className="site-title font-extrabold drop-shadow">Lisible</h1>
         <p className="site-subtitle mt-2 drop-shadow">Soutenir la littérature de demain</p>
