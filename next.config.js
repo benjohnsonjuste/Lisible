6c31085e7ebc83b9952aba1e809adb77badfe1e5
benjohnsonjuste <jb7management@gmail.com>
2025-09-24 01:06:04 -0400

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
-}
+  swcMinify: true,
+};
+
 module.exports = nextConfig;
