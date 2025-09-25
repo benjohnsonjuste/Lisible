3d0e4ecd087039e1b1493629b756e85daea59212
benjohnsonjuste <jb7management@gmail.com>
2025-09-25 00:02:51 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000
-  },
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
   server: {
     port: 4028,
     host: "0.0.0.0",
-    strictPort: true
-  }
+    strictPort: true,
+  },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
