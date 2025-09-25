f04ac80825164a1835925ba9074fbde3b4064737
benjohnsonjuste <jb7management@gmail.com>
2025-09-25 01:09:28 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000,
   },
   server: {
     port: 4028,
-    host: '0.0.0.0'
-  }
+    host: "0.0.0.0",
+    strictPort: true,
+  },
 });
