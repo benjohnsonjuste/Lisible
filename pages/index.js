b01c1d84296404fb9263061c099f16af3167e975
benjohnsonjuste <jb7management@gmail.com>
2025-09-25 23:34:32 -0400

    index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home(){
+  return (
+    <div className="text-center">
+      <img src="/couverture.jpg" alt="couverture" className="header-cover mb-6"/>
+      <h1 className="text-4xl font-bold">Lisible</h1>
+      <p className="text-lg text-gray-600 mt-2">Soutenir la littérature de demain.</p>
+    </div>
+  );
+}
