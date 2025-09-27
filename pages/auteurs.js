808bda883e4eb467c36a3a041306df3773e15f5e
benjohnsonjuste <jb7management@gmail.com>
2025-09-27 01:02:05 -0400

    auteurs.js

---
+++ pages/auteurs.js
@@ -0,0 +1,9 @@
+import AuthorsList from "@/components/AuthorsList";
+
+export default function AuthorsPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <AuthorsList />
+    </div>
+  );
+}
