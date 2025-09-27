104b06bdd1a9812d7bfbf81b8231115e6bcf9650
benjohnsonjuste <jb7management@gmail.com>
2025-09-27 16:43:49 -0400

    index.js

--- pages/evenements/index.js
+++ pages/evenements/index.js
@@ -1,21 +1,13 @@
-import Link from "next/link";
+// pages/battle-poetique.js
+import EventClosed from "@/components/EventClosed";
 
-export default function Evenements() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">Événements</h1>
-      <ul className="space-y-4">
-        <li>
-          <Link href="/evenements/foire-virtuelle" className="text-blue-600 underline">
-            Foire Virtuelle Cheikh Anta Diop
-          </Link>
-        </li>
-        <li>
-          <Link href="/evenements/battle-poetique" className="text-blue-600 underline">
-            Battle Poétique International
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-    }
+export default function BattlePoetiquePage() {
+  return <EventClosed eventName="Battle Poétique International" />;
+}
+
+// pages/foire-virtuale.js
+import EventClosed from "@/components/EventClosed";
+
+export default function FoireVirtualePage() {
+  return <EventClosed eventName="Foire Virtuelle Cheikh Anta Diop" />;
+}
