c6f5e6c3d893eda34cf225acc302d7c3ae85f6c2
benjohnsonjuste <jb7management@gmail.com>
2025-09-27 23:17:12 -0400

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 // next.config.js
 const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  dest: "public",            // Dossier où seront générés les fichiers PWA
+  register: true,            // Enregistre le service worker
+  skipWaiting: true,         // Remplace l'ancien service worker automatiquement
+  disable: process.env.NODE_ENV === "development", // Désactive PWA en mode dev
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
