1ebc386a7b26cf2356f7d1ace9a02142b3f33884
benjohnsonjuste <jb7management@gmail.com>
2025-09-27 22:01:58 -0400

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,21 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-function MyApp({ Component, pageProps }){
+function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Barre de navigation en haut */}
       <Navbar />
+
+      {/* Contenu principal */}
       <main className="container-md py-6">
         <Component {...pageProps} />
       </main>
+
+      {/* Footer affiché sur toutes les pages */}
+      <Footer />
     </>
   );
 }
+
 export default MyApp;
