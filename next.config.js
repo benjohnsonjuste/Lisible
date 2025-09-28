9961aec1720e5ba66e1f092dfc1e1b10b93003c0
benjohnsonjuste <jb7management@gmail.com>
2025-09-27 22:11:04 -0400

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // Désactive en mode dev
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // nothing experimental so compatible with Vercel
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
