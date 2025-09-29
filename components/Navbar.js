803e20e9644951756891750e7e20629842c21043
benjohnsonjuste <jb7management@gmail.com>
2025-09-29 00:36:14 -0400

    Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -132,7 +132,7 @@ export default function Navbar() {
             </li>
 
             <li>
-              <Link href="/lisible-club" legacyBehavior>
+              <Link href="/LisibleClub" legacyBehavior>
                 <a
                   onClick={handleMenuClick}
                   className="flex items-center gap-3 hover:text-blue-600"
