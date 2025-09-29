596deb289f60850fd5c0b13c2d330c9ab9378279
benjohnsonjuste <jb7management@gmail.com>
2025-09-28 23:45:52 -0400

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import InstallPrompt from "@/components/InstallPrompt";
+import { subscribeToClubPosts } from "@/lib/subscribeToClubPosts";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Abonnement automatique de l'utilisateur aux notifications
+    subscribeToClubPosts();
+  }, []);
+
   return (
     <>
       <Navbar />
