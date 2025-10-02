9f80e5b3bef47178dcddd700df1a77ff3055aad5
benjohnsonjuste <jb7management@gmail.com>
2025-10-02 01:41:39 -0500

    useServiceWorker.js

---
+++ hooks/useServiceWorker.js
@@ -0,0 +1,15 @@
+// useServiceWorker.js ou dans useEffect de App.jsx
+useEffect(() => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("Service Worker enregistré :", registration);
+        })
+        .catch((err) => {
+          console.error("Erreur d'enregistrement du SW :", err);
+        });
+    });
+  }
+}, []);
