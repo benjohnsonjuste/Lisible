1b1bc8ddddb7b01e5d407f1d02e81181e75de6f3
benjohnsonjuste <jb7management@gmail.com>
2025-10-03 01:13:04 -0400

    AppIcon.js

--- components/ui/AppIcon.js
+++ components/ui/AppIcon.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function AppIcon({ src, alt = "App Icon", className = "" }) {
   return (
     <img
-      src={src || "/public/logo.png"}
+      src={src || "/favicon.ico"}
       alt={alt}
       className={`w-10 h-10 object-contain ${className}`}
       onError={(e) => {
-        e.target.src = "/public/logo.png";
+        e.target.src = "/favicon.ico";
       }}
     />
   );
