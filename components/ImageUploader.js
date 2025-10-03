87fd53d03ebf5919b36ac371042eed5618cd3486
benjohnsonjuste <jb7management@gmail.com>
2025-10-03 00:15:12 -0400

    ImageUploader.js

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,7 +1,7 @@
 // components/ImageUploader.js
 import React, { useState, useRef } from "react";
 import AppIcon from "@/components/AppIcon";
-import Button from "@/components/ui/Button";
+import Bouton from "@/components/ui/Bouton";
 import Input from "@/components/ui/Input";
 import AppImage from "@/components/AppImage";
 
