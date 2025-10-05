c0e6cc69f36f3854ff2e773df04d5f19c8e4fea4
benjohnsonjuste <jb7management@gmail.com>
2025-10-05 09:18:37 -0400

    analytics.js

---
+++ pages/analytics.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import MetricsOverview from "@/components/MetricsOverview";
+
+export default function Analytics() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <MetricsOverview />
+    </div>
+  );
+}
