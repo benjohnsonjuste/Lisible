928241f51ecd658d48c8c09704298d4285a38501
benjohnsonjuste <jb7management@gmail.com>
2025-10-06 03:22:20 -0400

    index.js

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,16 +1,16 @@
 "use client";
 
 import React from "react";
-import PublishedTextsOverview from "@/components/PublishedTextsOverview";
+import TextLibrary from "@/components/TextLibrary";
 
-export default function PublishedTextsPage() {
+export default function library() {
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       <h1 className="text-2xl font-bold mb-6 text-gray-900">
         Tous les textes publiés
       </h1>
 
-      <PublishedTextsOverview />
+      <TextLibrary />
     </div>
   );
 }
