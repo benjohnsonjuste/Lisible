f90a0dde353c3e3f50a1e9545592ed79696602a2
benjohnsonjuste <jb7management@gmail.com>
2025-10-07 22:15:59 -0400

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,35 @@
+"use client";
+
 import "@/styles/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import InstallPrompt from "@/components/InstallPrompt";
 import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "sonner";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      {/* Barre de navigation */}
       <Navbar />
+
+      {/* Contenu principal */}
       <main className="container-md py-6">
         <Component {...pageProps} />
       </main>
+
+      {/* Bannière d’installation PWA */}
       <InstallPrompt />
+
+      {/* Pied de page */}
       <Footer />
-      <Analytics /> {/* ✅ Suivi Vercel Analytics */}
+
+      {/* Notifications (Toaster) */}
+      <Toaster position="top-right" richColors />
+
+      {/* Suivi Vercel Analytics */}
+      <Analytics />
     </AuthProvider>
   );
 }
