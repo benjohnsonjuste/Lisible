dc85088b8accce130fbe3f2380bfcba36555d910
benjohnsonjuste <jb7management@gmail.com>
2025-10-18 22:17:04 -0400

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
+// app/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // Optional: for authentication
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,6 +10,4 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app); // Optional: for authentication
+export const app = initializeApp(firebaseConfig);
