5e314f8f504734d398dce555524b332e5c306262
benjohnsonjuste <jb7management@gmail.com>
2025-10-19 23:08:35 -0400

    formatDate.js

---
+++ utils/formatDate.js
@@ -0,0 +1,8 @@
+export function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
