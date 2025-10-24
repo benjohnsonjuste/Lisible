71cb3c927c675c0d161f855bd49dd816dd39602f
benjohnsonjuste <jb7management@gmail.com>
2025-10-24 00:50:38 -0400

    NotificationBell.js

--- components/NotificationBell.js
+++ components/NotificationBell.js
@@ -6,13 +6,13 @@ import Link from "next/link";
 export default function NotificationBell() {
   return (
     <Link
-      href="/notifications/page.tsx"
+      href="/notifications"
       className="relative cursor-pointer hover:scale-110 transition-transform"
       title="Voir les notifications"
     >
       <Bell className="w-8 h-8 text-white hover:text-yellow-400 transition-colors" />
-      
-      {/* 🔔 Petit indicateur rouge si tu veux montrer qu'il y a des nouvelles notifications */}
+
+      {/* 🔔 Indicateur rouge si nouvelles notifications */}
       <span className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></span>
     </Link>
   );
