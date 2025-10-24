aee772fc85bf77199a8fa47eb93565ab8ceabd26
benjohnsonjuste <jb7management@gmail.com>
2025-10-24 18:00:48 -0400

    Delete pages/lisible-club directory

--- pages/lisible-club/index.js
+++
@@ -1,9 +0,0 @@
-import LisibleClubDashboard from "@/components/LisibleClubDashboard";
-
-export default function LisibleClub() {
-  return (
-    <div className="p-6">
-      <LisibleClubDashboard />
-    </div>
-  );
-}
