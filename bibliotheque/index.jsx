43994020d9cce0493efb39e072bc2d8857f3deb5
benjohnsonjuste <jb7management@gmail.com>
2025-10-25 02:03:07 -0400

    index.jsx

---
+++ bibliotheque/index.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import TextLibrary from "@/components/TextLibrary";
+
+export default function BibliothequePage() {
+  return (
+    <main className="max-w-7xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center">Bibliothèque Lisible</h1>
+      <TextLibrary />
+    </main>
+  );
+}
