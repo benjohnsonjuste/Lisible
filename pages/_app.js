42072d92c443895211dc6776a4bbfce34e5d1861
benjohnsonjuste <jb7management@gmail.com>
2025-10-25 17:39:59 -0400

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,25 @@ import Footer from "@/components/Footer";
 import InstallPrompt from "@/components/InstallPrompt";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "sonner";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        {/* Favicon pour navigateur */}
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.ico" sizes="16x16" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* Icônes PWA pour mobile */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+        <link rel="manifest" href="/favicon.ico" />
+
+        {/* Couleur de thème pour la barre du navigateur et notifications */}
+        <meta name="theme-color" content="#0d6efd" />
+      </Head>
+
       {/* Barre de navigation */}
       <Navbar />
 
