e37590e413485af7f30c828a32947019574f39f2
benjohnsonjuste <jb7management@gmail.com>
2025-10-25 02:34:54 -0400

    update-index.js

--- pages/api/update-index.js
+++ pages/api/update-index.js
@@ -77,4 +77,4 @@ export default async function handler(req, res) {
     console.error("update-index error:", err);
     return res.status(500).json({ error: err.message || "Erreur serveur" });
   }
-}
+} 
