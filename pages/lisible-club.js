62ec04821f9a0701565d28ad77c686a387fefdf9
benjohnsonjuste <jb7management@gmail.com>
2025-10-26 13:39:47 -0400

    lisible-club.js

---
+++ pages/lisible-club.js
@@ -0,0 +1,12 @@
+"use client";
+
+import LisibleClubClosed from "@/components/LisibleClubClosed";
+
+export default function ClubPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12 px-4">
+
+      <LisibleClubClosed />
+    </div>
+  );
+}
