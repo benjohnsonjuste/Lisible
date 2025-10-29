3f5d4c77155e5f2f427d08cb1a6754b791e7785a
benjohnsonjuste <jb7management@gmail.com>
2025-10-29 01:58:54 -0400

    Delete pages/auteurs.js

--- pages/auteurs.js
+++
@@ -1,9 +0,0 @@
-import AuthorsList from "@/components/AuthorsList";
-
-export default function AuthorsPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <AuthorsList />
-    </div>
-  );
-}
