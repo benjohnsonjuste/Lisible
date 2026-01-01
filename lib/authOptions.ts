030d889e2f9782c77127dee7a3bcc9abc4d64a5a
benjohnsonjuste <jb7management@gmail.com>
2026-01-01 13:03:30 -0500

    authOptions.ts

---
+++ lib/authOptions.ts
@@ -0,0 +1,25 @@
+import type { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt"
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email) return null
+
+        return {
+          id: "1",
+          email: credentials.email
+        }
+      }
+    })
+  ]
+}
