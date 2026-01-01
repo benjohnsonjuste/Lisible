c9d082388d0ebba0a781c539a8d0f965791f955a
benjohnsonjuste <jb7management@gmail.com>
2026-01-01 12:23:56 -0500

    comments.ts

---
+++ pages/api/comments.ts
@@ -0,0 +1,21 @@
+import { supabase } from "../../lib/supabase"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { text_id } = req.query
+
+  const { data, error } = await supabase
+    .from("comments")
+    .select("*")
+    .eq("text_id", text_id)
+    .order("created_at", { ascending: false })
+
+  if (error) {
+    return res.status(500).json({ error: error.message })
+  }
+
+  res.status(200).json(data)
+}
