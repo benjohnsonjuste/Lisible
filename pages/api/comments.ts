d78725727b7969909531fd1c8f14ab796105df5f
benjohnsonjuste <jb7management@gmail.com>
2026-01-01 12:33:05 -0500

    comments.ts

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,4 +1,4 @@
-import { supabase } from "../../lib/supabase"
+import { supabase } from "../lib/supabase"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(
