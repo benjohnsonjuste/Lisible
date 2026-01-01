f659710df41879adc0551ac67f4d7d99476d5ef8
benjohnsonjuste <jb7management@gmail.com>
2026-01-01 16:58:11 -0500

    authOptions.ts

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,25 +1,22 @@
-import type { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth, { AuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt"
-  },
+export const authOptions: AuthOptions = {
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email) return null
-
-        return {
-          id: "1",
-          email: credentials.email
-        }
-      }
-    })
-  ]
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub!
+      return session
+    },
+  },
 }
+
+export default NextAuth(authOptions)
