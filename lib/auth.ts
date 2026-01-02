cabd75456fd417eeb97ec206b4a4d1c72375e55d
benjohnsonjuste <jb7management@gmail.com>
2026-01-02 06:44:33 -0500

    auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,10 @@
 import { getServerSession } from "next-auth"
 import type { NextApiRequest, NextApiResponse } from "next"
-import { authOptions } from "@/lib/authOptions"
+import { authOptions } from "./authOptions"
 
-export async function requireAuth(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, authOptions)
-  if (!session) {
-    res.status(401).end()
-    return null
-  }
-  return session
+export async function requireAuth(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  return getServerSession(req, res, authOptions)
 }
