8d9fa7a947f7068375280e7f3ca763bab5dd1f68
benjohnsonjuste <jb7management@gmail.com>
2026-01-02 07:00:45 -0500

    [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,21 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/authOptions"
+import NextAuth, { type NextAuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+      }
+      return session
+    },
+  },
+}
 
 export default NextAuth(authOptions)
