2e03614b74b356e71736b7817003e7b23cd67c2c
benjohnsonjuste <jb7management@gmail.com>
2026-01-10 07:35:29 -0500

    authOptions.ts

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,15 +1,17 @@
-import type { NextAuthConfig } from "next-auth"
-import GitHubProvider from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github";
 
-export const authConfig: NextAuthConfig = {
+export const authConfig = {
   providers: [
-    GitHubProvider({
+    GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin", // page de connexion custom (optionnel)
+  },
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
-}
+};
