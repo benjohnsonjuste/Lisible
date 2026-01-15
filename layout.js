1db2afc6ed774fc67f3e6229651a45b396c2e043
benjohnsonjuste <jb7management@gmail.com>
2026-01-15 10:26:13 -0500

    layout.js

--- layout.js
+++ layout.js
@@ -10,7 +10,9 @@ export default function RootLayout({ children }) {
         <Header />
 
         {/* Contenu principal */}
-        <main className="pt-20 max-w-4xl mx-auto p-4">{children}</main>
+        <main className="pt-16 max-w-4xl mx-auto p-4">
+          {children}
+        </main>
       </body>
     </html>
   );
