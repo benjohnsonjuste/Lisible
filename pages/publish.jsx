06154fa2af5616b95f2ef51be6f082ff9d178432
benjohnsonjuste <jb7management@gmail.com>
2026-01-19 22:49:57 -0500

    publish.jsx

--- pages/publish.jsx
+++ pages/publish.jsx
@@ -44,11 +44,6 @@ export default function PublishPage() {
       }
 
       toast.success("Texte publié avec succès");
-
-      setTitle("");
-      setContent("");
-      setAuthorName("");
-
       router.push("/texts");
 
     } catch (err) {
