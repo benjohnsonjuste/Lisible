ce773208388f41bdcdd6c6c519e32c5196d802cb
Ben Johnson Juste <jb7management@gmail.com>
2026-02-04 14:57:20 -0500

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,23 +1,26 @@
-// next.config.js
+// next.config.cjs
 const withPWA = require("next-pwa")({
-  dest: "public",            // Dossier où seront générés les fichiers PWA
-  register: true,            // Enregistre le service worker
-  skipWaiting: true,         // Remplace l'ancien service worker automatiquement
-  disable: process.env.NODE_ENV === "development", // Désactive PWA en mode dev
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 1. Indispensable pour éviter l'erreur "client-side exception" avec Livepeer
   transpilePackages: ["@livepeer/react", "lucide-react"],
   
-  // 2. Autorise l'affichage des flux vidéo et des couvertures GitHub
   images: {
+    // Activation des formats modernes pour une vitesse incroyable
+    formats: ['image/avif', 'image/webp'],
+    // Unification des domaines autorisés
     domains: [
       "raw.githubusercontent.com", 
       "lp-playback.com", 
-      "livepeer.studio"
+      "livepeer.studio",
+      "api.dicebear.com",
+      "github.com"
     ],
   },
 };
