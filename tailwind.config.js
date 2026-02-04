b3951576863c2988b4898c0b903937a9cd4020dd
Ben Johnson Juste <jb7management@gmail.com>
2026-02-03 20:36:43 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media', // Ajout pour activer le Dark Mode automatique via préférence système
+  darkMode: 'class', // Permet le contrôle manuel ET la détection automatique
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
