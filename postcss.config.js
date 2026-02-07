5fefd3296f794cf3b4ec2999df21fa5f2e9d1b1b
Ben Johnson Juste <jb7management@gmail.com>
2026-02-07 17:09:03 -0500

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // Ajoute automatiquement les préfixes (-webkit, -moz, etc.) pour la compatibilité
+  },
+};
