f5b3ffb012af4e7a24f6fa9f479b7bcc33fdec62
Ben Johnson Juste <jb7management@gmail.com>
2026-02-07 22:48:11 -0500

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#0d9488", // Ton teal-600
+      },
+    },
+  },
+  plugins: [],
+};
