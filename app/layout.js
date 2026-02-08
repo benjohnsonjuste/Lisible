25c0463b9383b15fc4bf679b1da7e0919e7e3bcf
Ben Johnson Juste <jb7management@gmail.com>
2026-02-08 08:42:31 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,27 @@
 import "./globals.css";
 import { Toaster } from "sonner";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: 'Lisible - Publication',
+  title: 'Lisible - Lecture en streaming',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        {/* S'affiche sur toutes les pages */}
+        <Navbar />
+        
+        {/* Contenu principal qui pousse le footer vers le bas */}
+        <main className="flex-grow">
+          {children}
+        </main>
+        
+        {/* S'affiche sur toutes les pages */}
+        <Footer />
+        
         <Toaster position="top-center" richColors />
       </body>
     </html>
