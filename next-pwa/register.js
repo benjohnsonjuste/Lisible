1ea8c9452a06898ae315decac91e30f3559a4b38
Ben Johnson Juste <jb7management@gmail.com>
2026-02-07 21:11:16 -0500

    Delete register.js

--- next-pwa/register.js
+++
@@ -1,9 +0,0 @@
-import AuthDialog from "@/components/AuthDialog";
-export default function RegisterPage(){
-  return (
-    <div className="max-w-md mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Inscription</h2>
-      <AuthDialog type="register" />
-    </div>
-  );
-}
