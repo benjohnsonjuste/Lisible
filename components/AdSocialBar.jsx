dafae53f1a5df03d199d1bd22335e45c72ab0868
Ben Johnson Juste <jb7management@gmail.com>
2026-05-02 15:52:46 -0400

    Update AdSocialBar.jsx

--- components/AdSocialBar.jsx
+++ components/AdSocialBar.jsx
@@ -7,7 +7,7 @@ export default function AdSocialBar() {
     <Script
       id="ad-social-bar-script"
       src="https://pl28594689.profitablecpmratenetwork.com/62/bc/8f/62bc8f4d06d16b0f6d6297a4e94cfdfd.js"
-      strategy="afterInteractive"
+      strategy="afterInteractive" // Charge après que la page soit interactive
     />
   );
 }
