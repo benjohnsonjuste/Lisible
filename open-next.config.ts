e7fb45b9ac907f75bf63e3c0114dceacf0fea838
Ben Johnson Juste <jb7management@gmail.com>
2026-08-09 18:51:22 -0400

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,16 +1,6 @@
-import cache from "@opennextjs/cloudflare/kv-cache";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
 
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: async () => cache,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+});
